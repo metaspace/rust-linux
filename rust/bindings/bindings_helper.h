1a499fa72e9850df0ca477b7e85575a2e4de6201
FUJITA Tomonori <fujita.tomonori@gmail.com>
2022-08-05 05:27:24 +0000

    rust: add initial PCI support
    Added minimum abstration APIs for PCI.
    
    ---
    
    Original-author: FUJITA Tomonori <fujita.tomonori@gmail.com>
    
    Modified to apply to this tree.

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -12,6 +12,7 @@
 #include <linux/ethtool.h>
 #include <linux/jiffies.h>
 #include <linux/mdio.h>
+#include <linux/pci.h>
 #include <linux/phy.h>
 #include <linux/refcount.h>
 #include <linux/sched.h>
