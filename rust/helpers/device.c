35f852a4f893193bd244cda0ced480418fdf4554
Wedson Almeida Filho <wedsonaf@gmail.com>
2023-02-24 19:53:17 +0900

    rust: device: Add a stub abstraction for devices
    Add a Device type which represents an owned reference to a generic
    struct device. This minimal implementation just handles reference
    counting and allows the user to get the device name.
    
    Lina: Rewrote commit message, dropped the Amba bits, and squashed in
    simple changes to the core Device code from latter commits in
    rust-for-linux/rust. Also include the rust_helper_dev_get_drvdata
    helper which will be needed by consumers later on anyway.
    
    Co-developed-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Wedson Almeida Filho <wedsonaf@gmail.com>
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    
    ---
    
    Modified to apply to this tree.

---
+++ rust/helpers/device.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <linux/device.h>
+
+void *rust_helper_dev_get_drvdata(struct device *dev)
+{
+	return dev_get_drvdata(dev);
+}
+EXPORT_SYMBOL_GPL(rust_helper_dev_get_drvdata);
+
+const char *rust_helper_dev_name(const struct device *dev)
+{
+	return dev_name(dev);
+}
+EXPORT_SYMBOL_GPL(rust_helper_dev_name);
