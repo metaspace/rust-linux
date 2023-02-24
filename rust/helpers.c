d5dad3779a16c21d00af20767f4ea3bae8f172ea
Wedson Almeida Filho <wedsonaf@gmail.com>
2023-02-24 19:53:17 +0900

    rust: device: Add a stub abstraction for devices
    Add a Device type which represents an owned reference to a generic
    struct device. This minimal implementation just handles reference
    counting and allows the user to get the device name.
    
    Lina: Rewrote commit message, dropped the Amba bits, and squashed in
    simple changes to the core Device code from latter commits in
    rust-for-linux/rust. Also include the rust_helper_dev_get_drvdata
    helper which will be needed by consumers later on anyway.
    
    Co-developed-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Wedson Almeida Filho <wedsonaf@gmail.com>
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/helpers.c
+++ rust/helpers.c
@@ -23,6 +23,7 @@
 #include <kunit/test-bug.h>
 #include <linux/bug.h>
 #include <linux/build_bug.h>
+#include <linux/device.h>
 #include <linux/err.h>
 #include <linux/errname.h>
 #include <linux/mutex.h>
@@ -156,6 +157,18 @@ void rust_helper_init_work_with_key(struct work_struct *work, work_func_t func,
 	work->func = func;
 }
 EXPORT_SYMBOL_GPL(rust_helper_init_work_with_key);
+
+void *rust_helper_dev_get_drvdata(struct device *dev)
+{
+	return dev_get_drvdata(dev);
+}
+EXPORT_SYMBOL_GPL(rust_helper_dev_get_drvdata);
+
+const char *rust_helper_dev_name(const struct device *dev)
+{
+	return dev_name(dev);
+}
+EXPORT_SYMBOL_GPL(rust_helper_dev_name);
 
 /*
  * `bindgen` binds the C `size_t` type as the Rust `usize` type, so we can
