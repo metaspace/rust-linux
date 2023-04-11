b585f8d2733f15e534e8c1d25dd88736f07bab7c
Wedson Almeida Filho <walmeida@microsoft.com>
2023-04-11 02:45:37 -0300

    rust: lock: implement `IrqSaveBackend` for `SpinLock`
    This allows Rust code to use the `lock_irqsave` variant of spinlocks.
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Reviewed-by: Martin Rodriguez Reboredo <yakoyoku@gmail.com>
    Signed-off-by: Wedson Almeida Filho <walmeida@microsoft.com>

--- rust/helpers.c
+++ rust/helpers.c
@@ -224,6 +224,22 @@ void **rust_helper_radix_tree_next_slot(void **slot,
 }
 EXPORT_SYMBOL_GPL(rust_helper_radix_tree_next_slot);
 
+unsigned long rust_helper_spin_lock_irqsave(spinlock_t *lock)
+{
+	unsigned long flags;
+
+	spin_lock_irqsave(lock, flags);
+
+	return flags;
+}
+EXPORT_SYMBOL_GPL(rust_helper_spin_lock_irqsave);
+
+void rust_helper_spin_unlock_irqrestore(spinlock_t *lock, unsigned long flags)
+{
+	spin_unlock_irqrestore(lock, flags);
+}
+EXPORT_SYMBOL_GPL(rust_helper_spin_unlock_irqrestore);
+
 /*
  * `bindgen` binds the C `size_t` type as the Rust `usize` type, so we can
  * use it in contexts where Rust expects a `usize` like slice (array) indices.
