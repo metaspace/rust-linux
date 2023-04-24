2f3a2313e17a48714db11fac0005de5964d6187f
Andreas Hindborg <a.hindborg@samsung.com>
2023-04-24 14:43:53 +0200

    rust: add radix tree abstraction
    Add abstractions for the C radix_tree. This abstraction allows Rust code to use
    the radix_tree as a map from `u64` keys to `ForeignOwnable` values.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@samsung.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -11,6 +11,7 @@
 #include <linux/blk-mq.h>
 #include <linux/errname.h>
 #include <linux/slab.h>
+#include <linux/radix-tree.h>
 #include <linux/refcount.h>
 #include <linux/wait.h>
 #include <linux/sched.h>
@@ -19,4 +20,5 @@
 /* `bindgen` gets confused at certain things. */
 const size_t BINDINGS_ARCH_SLAB_MINALIGN = ARCH_SLAB_MINALIGN;
 const gfp_t BINDINGS_GFP_KERNEL = GFP_KERNEL;
+const gfp_t BINDINGS_GFP_ATOMIC = GFP_ATOMIC;
 const gfp_t BINDINGS___GFP_ZERO = __GFP_ZERO;
