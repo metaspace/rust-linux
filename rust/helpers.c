12a473c833db8b79ad010f18e0fdd9567c036681
Andreas Hindborg <a.hindborg@samsung.com>
2023-04-29 15:57:57 +0200

    rust: add `pages` module for handling page allocation
    This patch adds support for working with pages of order 0. Support for pages
    with higher order is deferred. Page allocation flags are fixed in this patch.
    Future work might allow the user to specify allocation flags.
    
    This patch is a heavily modified version of code available in the rust tree [1],
    primarily adding support for multiple page mapping strategies.
    
    [1] https://github.com/rust-for-Linux/linux/tree/bc22545f38d74473cfef3e9fd65432733435b79f/rust/kernel/pages.rs
    
    Signed-off-by: Andreas Hindborg <a.hindborg@samsung.com>

--- rust/helpers.c
+++ rust/helpers.c
@@ -25,6 +25,7 @@
 #include <linux/build_bug.h>
 #include <linux/err.h>
 #include <linux/errname.h>
+#include <linux/highmem.h>
 #include <linux/mutex.h>
 #include <linux/refcount.h>
 #include <linux/sched/signal.h>
@@ -143,6 +144,36 @@ struct kunit *rust_helper_kunit_get_current_test(void)
 	return kunit_get_current_test();
 }
 EXPORT_SYMBOL_GPL(rust_helper_kunit_get_current_test);
+
+void *rust_helper_kmap(struct page *page)
+{
+	return kmap(page);
+}
+EXPORT_SYMBOL_GPL(rust_helper_kmap);
+
+void rust_helper_kunmap(struct page *page)
+{
+	return kunmap(page);
+}
+EXPORT_SYMBOL_GPL(rust_helper_kunmap);
+
+void *rust_helper_kmap_atomic(struct page *page)
+{
+	return kmap_atomic(page);
+}
+EXPORT_SYMBOL_GPL(rust_helper_kmap_atomic);
+
+void rust_helper_kunmap_atomic(void *address)
+{
+	kunmap_atomic(address);
+}
+EXPORT_SYMBOL_GPL(rust_helper_kunmap_atomic);
+
+struct page *rust_helper_alloc_pages(gfp_t gfp_mask, unsigned int order)
+{
+	return alloc_pages(gfp_mask, order);
+}
+EXPORT_SYMBOL_GPL(rust_helper_alloc_pages);
 
 /*
  * `bindgen` binds the C `size_t` type as the Rust `usize` type, so we can
