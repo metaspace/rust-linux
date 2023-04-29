7c2d24bf13f94388216074692f336c8482ba29e2
Andreas Hindborg <a.hindborg@samsung.com>
2023-04-29 15:57:57 +0200

    IM: rust: add `pages` module for handling page allocation
    This patch adds support for working with pages of order 0. Support for pages
    with higher order is deferred. Page allocation flags are fixed in this patch.
    Future work might allow the user to specify allocation flags.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@samsung.com>
    
    ---
    
    This patch is a heavily modified version of code available in the rust tree [1],
    primarily adding support for multiple page mapping strategies.
    
    [1] https://github.com/rust-for-Linux/linux/tree/bc22545f38d74473cfef3e9fd65432733435b79f/rust/kernel/pages.rs

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -41,6 +41,7 @@ pub mod ioctl;
 pub mod kunit;
 #[cfg(CONFIG_NET)]
 pub mod net;
+pub mod pages;
 pub mod prelude;
 pub mod print;
 mod static_assert;
@@ -59,6 +60,11 @@ pub use uapi;
 
 #[doc(hidden)]
 pub use build_error::build_error;
+
+/// Page size defined in terms of the `PAGE_SHIFT` macro from C.
+///
+/// [`PAGE_SHIFT`]: ../../../include/asm-generic/page.h
+pub const PAGE_SIZE: u32 = 1 << bindings::PAGE_SHIFT;
 
 /// Prefix to appear before log messages printed from within the `kernel` crate.
 const __LOG_PREFIX: &[u8] = b"rust_kernel\0";
