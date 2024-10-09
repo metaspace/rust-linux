b7cd36776f08508878dfa36329871f6574756c4a
Andreas Hindborg <a.hindborg@kernel.org>
2024-10-09 13:34:43 +0200

    fixup: rcu

--- rust/helpers/rcu.c
+++ rust/helpers/rcu.c
@@ -5,11 +5,9 @@ void rust_helper_rcu_read_lock(void)
 {
 	rcu_read_lock();
 }
-EXPORT_SYMBOL_GPL(rust_helper_rcu_read_lock);
 
 void rust_helper_rcu_read_unlock(void)
 {
 	rcu_read_unlock();
 }
-EXPORT_SYMBOL_GPL(rust_helper_rcu_read_unlock);
 /* end rcu */
