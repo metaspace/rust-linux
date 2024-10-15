8ee70f8f7b00c2f7e21ac1e3c1e0e661d35d9edf
Andreas Hindborg <a.hindborg@kernel.org>
2024-10-15 14:16:50 +0200

    LIST(v3): rust: hrtimer: implement `TimerPointer` for `Arc`
    This patch allows the use of intrusive `hrtimer` fields in structs that are
    managed by an `Arc`.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>
    Pick: 20241017-hrtimer-v3-v6-12-rc2-v3-0-59a75cbb44da@kernel.org

--- rust/kernel/hrtimer.rs
+++ rust/kernel/hrtimer.rs
@@ -107,7 +107,6 @@ impl<T> Timer<T> {
     /// # Safety
     ///
     /// `self_ptr` must point to a valid `Self`.
-    #[allow(dead_code)]
     pub(crate) unsafe fn raw_cancel(self_ptr: *const Self) -> bool {
         // SAFETY: timer_ptr points to an allocation of at least `Timer` size.
         let c_timer_ptr = unsafe { Timer::raw_get(self_ptr) };
@@ -302,3 +301,5 @@ macro_rules! impl_has_timer {
         }
     }
 }
+
+mod arc;
