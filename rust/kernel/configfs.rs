b164c944452273a9f89f1c61072e9edd032957f8
Andreas Hindborg <a.hindborg@kernel.org>
2025-01-22 13:28:26 +0100

    add HasGroup

--- rust/kernel/configfs.rs
+++ rust/kernel/configfs.rs
@@ -1,33 +1,31 @@
-use core::{array::IntoIter, marker::PhantomData};
 use core::ptr::addr_of_mut;
+use core::{array::IntoIter, marker::PhantomData};
 use init::PinnedDrop;
 use kernel::alloc::flags;
 
 use crate::{prelude::*, types::Opaque};
 
 #[pin_data]
-pub struct Subsystem<A, G>
-{
+pub struct Subsystem<A, G, C> {
     #[pin]
     subsystem: Opaque<bindings::configfs_subsystem>,
-    _p: PhantomData<(A, G)>,
+    _p: PhantomData<(A, G, C)>,
 }
 
-unsafe impl<A, G> Sync for Subsystem<A,G>
-{}
+unsafe impl<A, G, C> Sync for Subsystem<A, G, C> {}
 
-unsafe impl<A, G> Send for Subsystem<A,G>
-{}
+unsafe impl<A, G, C> Send for Subsystem<A, G, C> {}
 
-impl<A, G> Subsystem<A, G>
+impl<A, G, C> Subsystem<A, G, C>
 where
-    A: AttributeOperations,
+    A: AttributeOperations<C>,
     G: GroupOperations,
+    C: HasGroup,
 {
     pub fn new(
         name: &'static CStr,
         owner: &ThisModule,
-        tpe: &'static ItemType<A, G>,
+        tpe: &'static ItemType<A, G, C>,
     ) -> impl PinInit<Self, Error> {
         try_pin_init!(Self {
             subsystem <- Opaque::try_ffi_init(|place: *mut bindings::configfs_subsystem| {
@@ -42,7 +40,8 @@ where
 }
 
 #[pin_data]
-struct Group {
+#[repr(transparent)]
+pub struct Group {
     #[pin]
     group: Opaque<bindings::config_group>,
 }
@@ -93,23 +92,29 @@ pub trait GroupOperations {
 }
 
 #[repr(transparent)]
-pub struct Attribute<T> {
+pub struct Attribute<AO, HG> {
     attribute: Opaque<bindings::configfs_attribute>,
-    _p: PhantomData<T>,
+    _p: PhantomData<(AO, HG)>,
 }
 
-unsafe impl<A> Sync for Attribute<A>
-{}
+unsafe impl<AO, HG> Sync for Attribute<AO, HG> {}
 
-unsafe impl<A> Send for Attribute<A>
-{}
+unsafe impl<AO, HG> Send for Attribute<AO, HG> {}
 
-impl<T> Attribute<T>
+impl<AO, HG> Attribute<AO, HG>
 where
-    T: AttributeOperations,
+    AO: AttributeOperations<HG>,
+    HG: HasGroup,
 {
-    unsafe extern "C" fn show(item: *mut bindings::config_item, page: *mut kernel::ffi::c_char) -> isize{
-        T::show(unsafe {&mut *(page as *mut [u8; 4096])})
+    unsafe extern "C" fn show(
+        item: *mut bindings::config_item,
+        page: *mut kernel::ffi::c_char,
+    ) -> isize {
+        let c_group: *mut bindings::config_group = item.cast();
+        let r_group_ptr: *mut Group = c_group.cast();
+        let container_ptr = unsafe {HG::container_ptr(r_group_ptr)};
+        let container_ref = unsafe { &*container_ptr };
+        AO::show(container_ref, unsafe { &mut *(page as *mut [u8; 4096]) })
     }
 
     unsafe extern "C" fn store(
@@ -117,26 +122,37 @@ where
         page: *const kernel::ffi::c_char,
         size: usize,
     ) -> isize {
-        T::store(unsafe {core::slice::from_raw_parts(page.cast(), size)})
+        let c_group: *mut bindings::config_group = item.cast();
+        let r_group_ptr: *mut Group = c_group.cast();
+        let container_ptr = unsafe {HG::container_ptr(r_group_ptr)};
+        let container_ref = unsafe { &*container_ptr };
+        AO::store(container_ref, unsafe {
+            core::slice::from_raw_parts(page.cast(), size)
+        })
     }
 
     pub const fn new(name: &'static CStr) -> Self {
         Self {
-            attribute: unsafe { Opaque::new(bindings::configfs_attribute {
-                ca_name: name as *const _ as _,
-                ca_owner: core::ptr::null_mut(),
-                ca_mode: 0o660,
-                show: Some(Self::show),
-                store: None,
-            })},
+            attribute: unsafe {
+                Opaque::new(bindings::configfs_attribute {
+                    ca_name: name as *const _ as _,
+                    ca_owner: core::ptr::null_mut(),
+                    ca_mode: 0o660,
+                    show: Some(Self::show),
+                    store: None,
+                })
+            },
             _p: PhantomData,
         }
     }
 }
 
-pub trait AttributeOperations {
-    fn show(page: &mut [u8; 4096]) -> isize;
-    fn store(page: &[u8]) -> isize;
+pub trait AttributeOperations<O>
+where
+    O: HasGroup,
+{
+    fn show(container: &O, page: &mut [u8; 4096]) -> isize;
+    fn store(container: &O, page: &[u8]) -> isize;
 }
 
 #[repr(transparent)]
@@ -145,30 +161,27 @@ unsafe impl<const N: usize> Send for AttributeList<N> {}
 unsafe impl<const N: usize> Sync for AttributeList<N> {}
 
 #[pin_data]
-pub struct ItemType<A, G>
-{
+pub struct ItemType<AO, GO, HG> {
     #[pin]
     item_type: Opaque<bindings::config_item_type>,
-    _p: PhantomData<(A,G)>,
+    _p: PhantomData<(AO, GO, HG)>,
 }
 
+unsafe impl<AO, GO, HG> Sync for ItemType<AO, GO, HG> {}
 
-unsafe impl<A, G> Sync for ItemType<A,G>
-{}
+unsafe impl<AO, GO, HG> Send for ItemType<AO, GO, HG> {}
 
-unsafe impl<A, G> Send for ItemType<A,G>
-{}
-
-impl<A, G> ItemType<A, G>
+impl<AO, GO, HG> ItemType<AO, GO, HG>
 where
-    A: AttributeOperations,
-    G: GroupOperations,
+    AO: AttributeOperations<HG>,
+    GO: GroupOperations,
+    HG: HasGroup,
 {
     pub const fn new<const N: usize>(attributes: &'static AttributeList<N>) -> Self {
         Self {
             item_type: Opaque::new(bindings::config_item_type {
                 ct_owner: core::ptr::null_mut(),
-                ct_group_ops: (&GroupOperationsVTable::<G>::VTABLE as *const _) as *mut _ ,
+                ct_group_ops: (&GroupOperationsVTable::<GO>::VTABLE as *const _) as *mut _,
                 ct_item_ops: core::ptr::null_mut(),
                 ct_attrs: attributes as *const _ as _,
                 ct_bin_attrs: core::ptr::null_mut(),
@@ -179,6 +192,20 @@ where
 
     fn as_ptr(&self) -> *const bindings::config_item_type {
         self.item_type.get()
+    }
+}
+
+pub unsafe trait HasGroup {
+    const OFFSET: usize;
+    unsafe fn group_ptr(self: *const Self) -> *const Group {
+        unsafe { self.cast::<u8>().add(Self::OFFSET).cast::<Group>() }
+    }
+
+    unsafe fn container_ptr(group: *mut Group) -> *mut Self
+    where
+        Self: Sized,
+    {
+        unsafe { group.cast::<u8>().sub(Self::OFFSET).cast::<Self>() }
     }
 }
 
--- samples/rust/rust_configfs.rs
+++ samples/rust/rust_configfs.rs
@@ -19,21 +19,27 @@ module! {
 #[pin_data]
 struct RustConfigfs {
     #[pin]
-    config: configfs::Subsystem<Self, Self>,
+    config: configfs::Subsystem<Self, Self, Self>,
+    msg: &'static CStr,
 }
 
 impl kernel::InPlaceModule for RustConfigfs {
     fn init(module: &'static ThisModule) -> impl PinInit<Self, Error> {
         pr_info!("Rust configfs sample (init)\n");
-        static ATTR: configfs::Attribute<RustConfigfs> = configfs::Attribute::new(c_str!("attr"));
+        static ATTR: configfs::Attribute<RustConfigfs, RustConfigfs> = configfs::Attribute::new(c_str!("attr"));
         static ATTRIBUTES: AttributeList<2> =
             AttributeList([&ATTR as *const _ as _, core::ptr::null_mut()]);
-        static TPE: configfs::ItemType<RustConfigfs, RustConfigfs> =
+        static TPE: configfs::ItemType<RustConfigfs, RustConfigfs, RustConfigfs> =
             configfs::ItemType::new(&ATTRIBUTES);
         try_pin_init!(Self {
             config <- configfs::Subsystem::new(c_str!("rust_configfs"), module, &TPE),
+            msg: c_str!("Hello World\n"),
         })
     }
+}
+
+fn show_msg(container: &RustConfigfs, page: &mut [u8; 4096]) -> isize {
+    todo!()
 }
 
 impl configfs::GroupOperations for RustConfigfs {
@@ -41,15 +47,19 @@ impl configfs::GroupOperations for RustConfigfs {
     fn drop_item() {}
 }
 
-impl configfs::AttributeOperations for RustConfigfs {
-    fn show(page: &mut [u8; 4096]) -> isize {
+impl configfs::AttributeOperations<RustConfigfs> for RustConfigfs {
+    fn show(container: &Self, page: &mut [u8; 4096]) -> isize {
         pr_info!("Show\n");
-        let data = c_str!("Hello World\n");
+        let data = container.msg;
         page[0..data.len()].copy_from_slice(data);
         data.len() as _
     }
-    fn store(page: &[u8]) -> isize {
+    fn store(container: &Self, page: &[u8]) -> isize {
         pr_info!("Store\n");
         page.len() as _
     }
+}
+
+unsafe impl configfs::HasGroup for RustConfigfs {
+    const OFFSET: usize = core::mem::offset_of!(Self, config) as usize;
 }
