6a52980852f6a8c234ad06d53e57eeb4c870f472
Andreas Hindborg <a.hindborg@kernel.org>
2025-01-30 14:13:00 +0100

    rust: rnull: enable configuration via `configfs`
    This patch allows null block devices to be configured and instantiated via
    `configfs`.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull.rs
+++ drivers/block/rnull.rs
@@ -10,6 +10,7 @@
 //!
 //! The driver is not configurable.
 
+use core::fmt::Write;
 use kernel::{
     alloc::flags,
     block::mq::{
@@ -17,9 +18,15 @@ use kernel::{
         gen_disk::{self, GenDisk},
         Operations, TagSet,
     },
+    c_str,
+    configfs::{self, Group},
+    configfs_attrs,
     error::Result,
-    new_mutex, pr_info,
+    new_mutex,
+    page::PAGE_SIZE,
+    pr_info,
     prelude::*,
+    str::CString,
     sync::{Arc, Mutex},
     types::ARef,
 };
@@ -32,25 +39,124 @@ module! {
     license: "GPL v2",
 }
 
+#[pin_data]
 struct NullBlkModule {
-    _disk: Pin<KBox<Mutex<GenDisk<NullBlkDevice>>>>,
+    #[pin]
+    configfs_subsystem: configfs::Subsystem<Config>,
 }
 
-impl kernel::Module for NullBlkModule {
-    fn init(_module: &'static ThisModule) -> Result<Self> {
+impl kernel::InPlaceModule for NullBlkModule {
+    fn init(_module: &'static ThisModule) -> impl PinInit<Self, Error> {
         pr_info!("Rust null_blk loaded\n");
-        let tagset = Arc::pin_init(TagSet::new(1, 256, 1), flags::GFP_KERNEL)?;
+        let item_type = configfs_attrs! {
+            container: Config,
+            child: DeviceConfig,
+            pointer: Arc<configfs::Group<DeviceConfig>>,
+            pinned: Arc<configfs::Group<DeviceConfig>>,
+            attributes: [],
+        };
 
-        let disk = gen_disk::GenDiskBuilder::new()
-            .capacity_sectors(4096 << 11)
-            .logical_block_size(4096)?
-            .physical_block_size(4096)?
-            .rotational(false)
-            .build(format_args!("rnullb{}", 0), tagset)?;
+        try_pin_init!(Self {
+            configfs_subsystem <- configfs::Subsystem::new(c_str!("rnull"), item_type, try_pin_init!(Config {}))
+        })
+    }
+}
 
-        let disk = KBox::pin_init(new_mutex!(disk, "nullb:disk"), flags::GFP_KERNEL)?;
+#[pin_data]
+struct Config {}
 
-        Ok(Self { _disk: disk })
+#[vtable]
+impl
+    configfs::GroupOperations for Config
+{
+    type Parent = Config;
+    type Child = DeviceConfig;
+    type ChildPointer = Arc<Group<DeviceConfig>>;
+    type PinChildPointer = Arc<Group<DeviceConfig>>;
+
+    fn make_group(
+        _this: &Config,
+        name: &CStr,
+    ) -> Result<impl PinInit<configfs::Group<DeviceConfig>, Error>> {
+        let item_type = configfs_attrs! {
+            container: DeviceConfig,
+            attributes: [
+                powered: 0,
+            ],
+        };
+
+        Ok(configfs::Group::new(
+            name.try_into()?,
+            item_type,
+            // TODO: cannot coerce new_mutex!() to impl PinInit<_, Error>, so put mutex inside
+            try_pin_init!( DeviceConfig {
+                data <- new_mutex!( DeviceConfigInner {
+                    powered: false,
+                    disk: None,
+                    name: name.try_into()?,
+                }),
+            }),
+        ))
+    }
+}
+
+#[pin_data]
+struct DeviceConfig {
+    #[pin]
+    data: Mutex<DeviceConfigInner>,
+}
+
+#[pin_data]
+struct DeviceConfigInner {
+    powered: bool,
+    name: CString,
+    disk: Option<GenDisk<NullBlkDevice>>,
+}
+
+#[vtable]
+impl configfs::AttributeOperations<0> for DeviceConfig {
+    type Data = DeviceConfig;
+
+    fn show(this: &DeviceConfig, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
+        pr_info!("Show powered\n");
+        let mut writer = kernel::str::BufferWriter::new(page)?;
+
+        if this.data.lock().powered {
+            writer.write_fmt(fmt!("1\n"))?;
+        } else {
+            writer.write_fmt(fmt!("0\n"))?;
+        }
+
+        Ok(writer.pos())
+    }
+
+    fn store(this: &DeviceConfig, page: &[u8]) -> Result {
+        let power_op: bool = core::str::from_utf8(page)?
+            .trim()
+            .parse::<u8>()
+            .map_err(|_| kernel::error::code::EINVAL)?
+            != 0;
+
+        let mut guard = this.data.lock();
+
+        if !guard.powered && power_op {
+            let tagset = Arc::pin_init(TagSet::new(1, 256, 1), flags::GFP_KERNEL)?;
+
+            let disk = gen_disk::GenDiskBuilder::new()
+                .capacity_sectors(4096 << 11)
+                .logical_block_size(4096)?
+                .physical_block_size(4096)?
+                .rotational(false)
+                .build(fmt!("{}", guard.name.to_str()?), tagset)?;
+
+            guard.disk = Some(disk);
+            guard.powered = true;
+        } else if guard.powered && !power_op {
+            drop(guard.disk.take());
+            guard.powered = false;
+        }
+
+        Ok(())
     }
 }
 
